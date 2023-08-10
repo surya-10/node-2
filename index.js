907696f0377a670daa06bcbcec85c0b8f71afdd8
surya-10 <sp659151@gmail.com>
2023-08-10 13:14:21 +0530

    final

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-let express = require("express");
+import express from "express";
+import cors from "cors";
+import {rooms} from "./rooms.js"
+
 let app = express();
 
-
-let cors = require("cors");
 
 app.use(express.json());
 app.use(cors());
@@ -11,205 +12,6 @@ let port = 9000;
 app.listen(port, ()=>{
     console.log(`Connected to port: ${port}`);
 })
-
-
-
-let rooms = [
-    {
-        id:0,
-        room_no:600,
-        pricePerHour:500,
-        amentities:["AC", "washing machine", "Telephone"],
-        isBooked:false,
-        roomName:"Single Room",
-        seatsAvailable:5,
-        customerName:"",
-        startTime:"",
-        endTime:"",
-        date:"",
-        bookingstatus:"not booked",
-        bookingDate:""
-    },
-    {
-        id:1,
-        room_no:601,
-        pricePerHour:600,
-        amentities:["AC", "washing machine", "Telephone", "Television"],
-        isBooked:false,
-        roomName:"Single Room",
-        seatsAvailable:6,
-        customerName:"",
-        startTime:"",
-        endTime:"",
-        date:"",
-        bookingstatus:"not booked",
-        bookingDate:""
-    },
-    {
-        id:2,
-        room_no:602,
-        pricePerHour:400,
-        amentities:["washing machine", "Telephone"],
-        isBooked:false,
-        roomName:"Single Room",
-        seatsAvailable:4,
-        customerName:"",
-        startTime:"",
-        endTime:"",
-        date:"",
-        bookingstatus:"not booked",
-        bookingDate:""
-    },
-    {
-        id:3,
-        room_no:605,
-        pricePerHour:900,
-        amentities:["AC", "washing machine", "Telephone", "AC"],
-        isBooked:true,
-        roomName:"Double Room",
-        seatsAvailable:7,
-        customerName:"vijay",
-        startTime:"5PM",
-        endTime:"12AM",
-        date:"15-10-2023",
-        bookingstatus:"booked",
-        bookingDate:"8-8-2023"
-    },
-    {
-        id:4,
-        room_no:606,
-        pricePerHour:1100,
-        amentities:["AC", "washing machine", "Telephone", "AC", "Fridge"],
-        isBooked:false,
-        roomName:"Double Room",
-        seatsAvailable:9,
-        customerName:"",
-        startTime:"",
-        endTime:"",
-        date:"",
-        bookingstatus:"not booked",
-        bookingDate:""
-    },
-    {
-        id:5,
-        room_no:607,
-        pricePerHour:1100,
-        amentities:["AC", "washing machine", "Telephone", "AC", "Fridge"],
-        isBooked:true,
-        roomName:"Double Room",
-        seatsAvailable:11,
-        customerName:"arthy",
-        startTime:"10AM",
-        endTime:"12PM",
-        date:"29-08-2023",
-        bookingstatus:"pending payment",
-        bookingDate:"01-08-2022"
-    },
-    {
-        id:6,
-        room_no:608,
-        pricePerHour:1000,
-        amentities:["AC", "Telephone", "AC", "Fridge"],
-        isBooked:true,
-        roomName:"Double Room",
-        seatsAvailable:3,
-        customerName:"arthy",
-        startTime:"8AM",
-        endTime:"12PM",
-        date:"16-11-2023",
-        bookingstatus:"booked",
-        bookingDate:"10-09-2023"
-    },
-    {
-        id:7,
-        room_no:609,
-        pricePerHour:1100,
-        amentities:["AC", "washing machine", "Telephone", "AC", "Fridge"],
-        isBooked:true,
-        roomName:"Double Room",
-        seatsAvailable:8,
-        customerName:"sanjay",
-        startTime:"10AM",
-        endTime:"5PM",
-        date:"15-10-2023",
-        bookingstatus:"payment pending",
-        bookingDate:"15-07-2023"
-    },
-    {
-        id:8,
-        room_no:610,
-        pricePerHour:1500,
-        amentities:["AC", "washing machine", "Telephone", "AC", "Fridge", "Luggages storage"],
-        isBooked:false,
-        roomName:"Family room",
-        seatsAvailable:15,
-        customerName:"",
-        startTime:"",
-        endTime:"",
-        date:"",
-        bookingstatus:"not booked"
-    },
-    {
-        id:9,
-        room_no:611,
-        pricePerHour:1800,
-        amentities:["AC", "washing machine", "Telephone", "AC", "Fridge", "Luggages storage", "extra bed"],
-        isBooked:false,
-        roomName:"Family room",
-        seatsAvailable:13,
-        customerName:"",
-        startTime:"",
-        endTime:"",
-        date:"",
-        bookingstatus:"not booked"
-    },
-    {
-        id:10,
-        room_no:612,
-        pricePerHour:1600,
-        amentities:["AC", "washing machine", "Telephone", "AC", "Fridge"],
-        isBooked:false,
-        roomName:"Family room",
-        seatsAvailable:13,
-        customerName:"",
-        startTime:"",
-        endTime:"",
-        date:"",
-        bookingstatus:"not booked"
-    },
-    {
-        id:11,
-        room_no:603,
-        pricePerHour:700,
-        amentities:["washing machine", "Telephone", "AC"],
-        isBooked:true,
-        roomName:"Single Room",
-        seatsAvailable:2,
-        customerName:"pavithra",
-        startTime:"12AM",
-        endTime:"12PM",
-        date:"17-11-2023",
-        bookingstatus:"booked",
-        bookingDate:"11-11-2023"
-    },
-    {
-        id:12,
-        room_no:604,
-        pricePerHour:650,
-        amentities:["Telephone", "AC"],
-        isBooked:true,
-        roomName:"Single Room",
-        seatsAvailable:3,
-        customerName:"bala",
-        startTime:"9AM",
-        endTime:"1PM",
-        date:"10-12-2023",
-        bookingstatus:"payment pending",
-        bookingDate:"19-10-2023"
-    }
-
-]
-
 app.get("/", (req, res)=>{
     if(!rooms){
         res.status(400).send({message:"We dont to have rooms"})
@@ -250,9 +52,9 @@ app.post("/book-room", (req, res)=>{
         data.seatsAvailable = roomData.seatsAvailable;
         data.bookingstatus="booked"
     }
-    let dat = [data];
+    var dat = [data];
     let final = rooms.splice(ind, 1);
-    rooms = [...rooms, ...dat];
+    rooms.push(...dat);
     // console.log(rooms);
     return res.send({bookedRoom:data});
 })
